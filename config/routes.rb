32727f356e1ce9a81e677e230429280606e77719
Gavin Stark <g.stark@realdigitalmedia.com>
2009-08-22 09:13:35 -0400

    routes set for POST

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.root :controller => "application"
 
-  map.create_omnom '/', :controller => "application", :action => "create_omnom"
+  map.create_omnom '/',                        :controller => "application", :action => "create_omnom", :conditions => { :method => :post }
+  map.vote         '/vote/:verification_code', :controller => "application", :aciton => "vote",         :conditions => { :method => :post }
 end
