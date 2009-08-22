b50a7658e6fd94795d2bace27dc94d25ad64d2f6
Matt Cowley <madcowley@gmail.com>
2009-08-22 11:15:35 -0400

    err..fix the table name in migration

--- db/migrate/20090822143839_add_name_to_ppl.rb
+++ db/migrate/20090822143839_add_name_to_ppl.rb
@@ -1,9 +1,9 @@
 class AddNameToPpl < ActiveRecord::Migration
   def self.up
-    add_column :ppl, :name, :string, :null => true
+    add_column :pplz, :name, :string, :null => true
   end
 
   def self.down
-    remove_column :ppl, :name
+    remove_column :pplz, :name
   end
 end
