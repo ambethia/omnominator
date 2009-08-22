516dd029cd8e4bc9357cac1e7805b271ca61433f
Jason L Perry <jasper@ambethia.com>
2009-08-21 20:51:28 -0400

    RSpec

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -5,3 +5,6 @@ config.action_controller.perform_caching             = false
 config.action_view.cache_template_loading            = true
 config.action_controller.allow_forgery_protection    = false
 config.action_mailer.delivery_method                 = :test
+
+config.gem "rspec",       :lib => false, :version => ">= 1.2.0"
+config.gem "rspec-rails", :lib => false, :version => ">= 1.2.0"
