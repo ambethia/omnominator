201438afc9af00c7b3638cecb214aa949e1b519a
Matt Cowley <madcowley@gmail.com>
2009-08-22 11:44:16 -0400

    add separate email for creator verification

--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -1,5 +1,13 @@
 class Mailer < ActionMailer::Base
   
+  def creator_verification(omnom, ppl_email)
+    @recipients         = ppl_email
+    @subject            = "Activate your Omnominator!"
+    @sent_on            = Time.now
+    @body[:ppl_email]   = ppl_email
+    @body[:url]         = "http://failblog.com" #url_for(:host => host, :action=>"index", :controller=>"home")
+  end
+  
   def vote_invitation(omnom, ppl_email)
     @recipients         = ppl_email
     @subject            = "You've been asked to pick a place to eat on OmNominator"
