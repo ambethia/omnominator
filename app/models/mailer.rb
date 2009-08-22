1a83a01861858f36291d4d035865bd4e4a95b04f
Gavin Stark <g.stark@realdigitalmedia.com>
2009-08-22 01:31:17 -0400

    Specs for email generation. Booyah!

--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -1,11 +1,10 @@
 class Mailer < ActionMailer::Base
   
-  def vote_invitation(ballot, voter, host)
-    @recipients = voter.email
-    @subject = "You've been asked to pick a place to eat on OmNominator"
-    @sent_on = Time.now
-    @body[:host] = host
-    @body[:voter] = voter
-    @body[:url] = "http://failblog.com" #url_for(:host => host, :action=>"index", :controller=>"home")
+  def vote_invitation(ballot, voter_email)
+    @recipients         = voter_email
+    @subject            = "You've been asked to pick a place to eat on OmNominator"
+    @sent_on            = Time.now
+    @body[:voter_email] = voter_email
+    @body[:url]         = "http://failblog.com" #url_for(:host => host, :action=>"index", :controller=>"home")
   end
 end
