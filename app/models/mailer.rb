02cd966b101073a979760db7efaf57567ce95e78
Jason L Perry <jasper@ambethia.com>
2009-08-23 10:24:48 -0400

    ActionMailer are configurated

--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -2,6 +2,7 @@ class Mailer < ActionMailer::Base
   
   def creator_verification(ppl)
     @recipients         = ppl.email
+    @from               = ["Omnominator","no-reply@omnominator.com"]
     @subject            = "Activate your Omnominator!"
     @sent_on            = Time.now
     @body[:ppl]         = ppl
@@ -9,9 +10,11 @@ class Mailer < ActionMailer::Base
   
   def vote_invitation(omnom, ppl)
     @recipients         = ppl.email
+    @from               = ["Omnominator","no-reply@omnominator.com"]
+    @reply_to           = omnom.creator.email
     @subject            = "You've been asked to pick a place to eat on OmNominator"
     @sent_on            = Time.now
     @body[:ppl]         = ppl
-    @body[:creator]     = omnom.creator.name.nil? ? omnom.creator.email : omnom.creator.name
+    @body[:creator]     = omnom.creator.name ? omnom.creator.email : omnom.creator.name
   end
 end
