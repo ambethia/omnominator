5f0499c78ca7023dabc0be7bf8d50d470c2c2371
James Basom Seaman II <jamesbasom@gmail.com>
2009-08-23 18:38:04 -0400

    Resuts page donz

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -32,7 +32,7 @@ module ApplicationHelper
   end
 
   def gravatar_image(email)
-    image_tag(Gravatar.new(email, { :default => "http://#{request.host_with_port}/images/default_gravatar.png" }).url)
+    image_tag(Gravatar.new(email, { :default => "http://#{request.host_with_port}/images/default_gravatar.png", :size => 32 }).url, :size => "32x32")
   end
 
   def flash_script
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,3 +5,5 @@ config.action_view.debug_rjs                         = true
 config.action_controller.perform_caching             = false
 config.action_mailer.raise_delivery_errors           = true
 config.action_mailer.default_url_options             = { :host => "0.0.0.0:3000" }
+
+config.gem "ambethia-smtp-tls", :lib => "smtp-tls"
