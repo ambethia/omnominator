d356da9dc5c0e4a251e8df255236972b30ccf8da
Jason L Perry <jasper@ambethia.com>
2009-10-09 16:51:02 -0400

    Use different from address in mailer

--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -1,16 +1,16 @@
 class Mailer < ActionMailer::Base
-  
+
   def creator_verification(ppl)
     @recipients         = ppl.email
-    @from               = ["Omnominator","no-reply@omnominator.com"]
+    @from               = ["Omnominator","noreply@omnominator.com"]
     @subject            = "Activate your Omnominator!"
     @sent_on            = Time.now
     @body[:ppl]         = ppl
   end
-  
+
   def vote_invitation(omnom, ppl)
     @recipients         = ppl.email
-    @from               = ["Omnominator","no-reply@omnominator.com"]
+    @from               = ["Omnominator","noreply@omnominator.com"]
     @reply_to           = omnom.creator.email
     @subject            = "You've been asked to pick a place to eat on OmNominator"
     @sent_on            = Time.now
